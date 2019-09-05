ce3638c2668ea3152b996df03c13bc09d5329900
Drzaln <drzaln@null.net>
2019-09-06 04:51:52 +0700

    deploy

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Home from '../screens/Home'
 import Login from '../screens/Login'
 
 export class Routing extends Component {
   render () {
     return (
-      <Router>
+      <div>
         <Route exact path='/' component={Login} />
         <Route exact path='/home' component={Home} />
-      </Router>
+      </div>
     )
   }
 }
